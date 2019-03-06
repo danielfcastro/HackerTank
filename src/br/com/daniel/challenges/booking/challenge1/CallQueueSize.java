dbd70944a17f964d0f0338f7c3009295b7ff8b96
daniel_ferreira <daniel_ferreira@MZ-MOVEL-104.GDB.NET>
2019-03-06 14:37:06 -0300

    Não funcional.  Precisa concluir.

---
+++ src/br/com/daniel/challenges/booking/challenge1/CallQueueSize.java
@@ -0,0 +1,17 @@
+package br.com.daniel.challenges.booking.challenge1;
+
+import java.util.List;
+
+public class CallQueueSize {
+	int on;
+	class Call {
+		public int start;
+		public int end;
+	}
+	
+	public count(List<Call> calls) {
+		for(Call e : calls) {
+			
+		}
+	}
+}
---
+++ src/br/com/daniel/challenges/booking/challenge1/SortHotelsList.java
@@ -0,0 +1,9 @@
+package br.com.daniel.challenges.booking.challenge1;
+
+import java.util.List;
+
+public class SortHotelsList {
+	static List<Integer> sort_hotels(String keywords, List<Integer> hotel_ids, List<String> reviews) {
+		
+	}
+}
